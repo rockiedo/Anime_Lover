04e442d4f2b45adce9110de2b97aaac0b66fc764
thachdd <ddthach95@gmail.com>
2017-02-08 22:46:37 +0700

    Using VideoView to load video.

--- app/src/main/java/thachdd/vuighenet/activity/PlayerActivity.java
+++ app/src/main/java/thachdd/vuighenet/activity/PlayerActivity.java
@@ -2,14 +2,17 @@ package thachdd.vuighenet.activity;
 
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.MediaController;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.wang.avi.AVLoadingIndicatorView;
 
@@ -19,9 +22,8 @@ import thachdd.vuighenet.api_client.ApiInterface;
 import thachdd.vuighenet.api_client.PlayerCallback;
 
 public class PlayerActivity extends AppCompatActivity implements SurfaceHolder.Callback, MediaPlayer.OnPreparedListener {
-    private SurfaceView mSurfaceView = null;
-    private SurfaceHolder mSurfaceHolder = null;
-    private MediaPlayer mMediaPlayer = null;
+    private VideoView mVideoView = null;
+    private MediaController mMediaController = null;
 
     private RelativeLayout mLoadingContainer = null;
     private AVLoadingIndicatorView mLoading = null;
@@ -31,7 +33,7 @@ public class PlayerActivity extends AppCompatActivity implements SurfaceHolder.C
     public void onWindowFocusChanged(boolean hasFocus) {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
-            mSurfaceView.setSystemUiVisibility(
+            mVideoView.setSystemUiVisibility(
                     View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                             | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                             | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -46,29 +48,32 @@ public class PlayerActivity extends AppCompatActivity implements SurfaceHolder.C
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        mSurfaceView = (SurfaceView) findViewById(R.id.player_surfaceView);
-        mSurfaceHolder = mSurfaceView.getHolder();
-        mSurfaceHolder.addCallback(this);
+        mVideoView = (VideoView) findViewById(R.id.player_videoview);
+        mMediaController = new MediaController(this);
+        mMediaController.setAnchorView(mVideoView);
+        mVideoView.setMediaController(mMediaController);
 
         mLoadingContainer = (RelativeLayout) findViewById(R.id.player_loading_container);
         mLoading = (AVLoadingIndicatorView) findViewById(R.id.player_loading);
 
 //        showLoading(true);
+        loadVideo();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        mMediaPlayer.stop();
+        mVideoView.stopPlayback();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        mMediaPlayer.release();
-        mMediaPlayer = null;
+        if (mVideoView != null) {
+            mVideoView = null;
+        }
     }
 
     public void playerOnClick(View v) {
@@ -90,7 +95,7 @@ public class PlayerActivity extends AppCompatActivity implements SurfaceHolder.C
 
     public void onPlayerLoadedSuccessfully(String url) {
 //        url = "https://archive.org/download/ksnn_compilation_master_the_internet/ksnn_compilation_master_the_internet_512kb.mp4";
-        url = "https://redirector.googlevideo.com/videoplayback?id=7732952c93ac3251&itag=59&source=webdrive&requiressl=yes&ttl=transient&mm=30&mn=sn-npoe7n7y&ms=nxu&mv=m&pl=20&mime=video/mp4&lmt=1477820339908422&mt=1486565919&ip=139.59.229.244&ipbits=0&expire=1486580339&sparams=ip%2Cipbits%2Cexpire%2Cid%2Citag%2Csource%2Crequiressl%2Cttl%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cmime%2Clmt&signature=8F971E60538F2DAFF97A346568AEF066976547EA.7D002483A045F10A0D2B75F2923D170E36E759B3&key=ck2&app=explorer&title=[VuiGhe.Net] Vua Hai Tac - Tap 0-(480p)";
+//        url = "https://redirector.googlevideo.com/videoplayback?id=7732952c93ac3251&itag=59&source=webdrive&requiressl=yes&ttl=transient&mm=30&mn=sn-npoe7n7y&ms=nxu&mv=m&pl=20&mime=video/mp4&lmt=1477820339908422&mt=1486565919&ip=139.59.229.244&ipbits=0&expire=1486580339&sparams=ip%2Cipbits%2Cexpire%2Cid%2Citag%2Csource%2Crequiressl%2Cttl%2Cmm%2Cmn%2Cms%2Cmv%2Cpl%2Cmime%2Clmt&signature=8F971E60538F2DAFF97A346568AEF066976547EA.7D002483A045F10A0D2B75F2923D170E36E759B3&key=ck2&app=explorer&title=[VuiGhe.Net] Vua Hai Tac - Tap 0-(480p)";
 
         if (url == null) {
             Toast.makeText(this, "Khong the tai video", Toast.LENGTH_LONG).show();
@@ -100,16 +105,9 @@ public class PlayerActivity extends AppCompatActivity implements SurfaceHolder.C
             return;
         }
 
-        try {
-            mMediaPlayer.setDataSource(url);
-            mMediaPlayer.prepareAsync();
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
-            Toast.makeText(this, "Khong the tai video", Toast.LENGTH_LONG).show();
-            showLoading(false);
-            finish();
-        }
+        Uri uri = Uri.parse(url);
+        mVideoView.setVideoURI(uri);
+        mVideoView.start();
     }
 
     public void onPlayerLoadedFailed() {
@@ -132,17 +130,17 @@ public class PlayerActivity extends AppCompatActivity implements SurfaceHolder.C
     @Override
     public void onPrepared(MediaPlayer mp) {
         showLoading(false);
-        mMediaPlayer.start();
+//        mMediaPlayer.start();
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
-        mMediaPlayer = new MediaPlayer();
-        mMediaPlayer.setDisplay(mSurfaceHolder);
-        mMediaPlayer.setOnPreparedListener(this);
+//        mMediaPlayer = new MediaPlayer();
+//        mMediaPlayer.setDisplay(mSurfaceHolder);
+//        mMediaPlayer.setOnPreparedListener(this);
 //        mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-        loadVideo();
+//
+//        loadVideo();
     }
 
     @Override

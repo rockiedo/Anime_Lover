6542f64e36abeb0fb9d9065371b2ff2fdbf5efe1
thachdd <ddthach95@gmail.com>
2017-02-21 18:01:46 +0700

    Using Exo Player.

--- app/src/main/java/thachdd/vuighenet/activity/PlayerActivity.java
+++ app/src/main/java/thachdd/vuighenet/activity/PlayerActivity.java
@@ -1,17 +1,31 @@
 package thachdd.vuighenet.activity;
 
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.MediaController;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
-import android.widget.VideoView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveVideoTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.wang.avi.AVLoadingIndicatorView;
 
 import thachdd.vuighenet.R;
@@ -19,42 +33,29 @@ import thachdd.vuighenet.api_client.ApiClient;
 import thachdd.vuighenet.api_client.HerokuInterface;
 import thachdd.vuighenet.api_client.PlayerCallback;
 
-public class PlayerActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener, MediaPlayer.OnErrorListener {
-    private VideoView mVideoView = null;
-    private MediaController mMediaController = null;
+public class PlayerActivity extends AppCompatActivity {
+    private SimpleExoPlayerView mExoView = null;
+    private SimpleExoPlayer mExoPlayer = null;
 
     private RelativeLayout mLoadingContainer = null;
     private AVLoadingIndicatorView mLoading = null;
     private PlayerCallback mPlayerCallback = null;
 
     @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-
-        Log.d("mylog", "onWindowFocusChanged(): " + hasFocus);
-
-        if (hasFocus) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
-        }
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        mVideoView = (VideoView) findViewById(R.id.player_videoview);
-        mMediaController = new MediaController(this);
-        mMediaController.setAnchorView(mVideoView);
-        mVideoView.setMediaController(mMediaController);
+        mExoView = (SimpleExoPlayerView) findViewById(R.id.main_exoview);
 
-        mVideoView.setOnPreparedListener(this);
-        mVideoView.setOnErrorListener(this);
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory = new AdaptiveVideoTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        LoadControl loadControl = new DefaultLoadControl();
+
+        mExoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector, loadControl);
+        mExoView.setPlayer(mExoPlayer);
 
         mLoadingContainer = (RelativeLayout) findViewById(R.id.player_loading_container);
         mLoading = (AVLoadingIndicatorView) findViewById(R.id.player_loading);
@@ -63,36 +64,33 @@ public class PlayerActivity extends AppCompatActivity implements MediaPlayer.OnP
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
 
-        if (mVideoView != null) {
-            mVideoView.resume();
-        }
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-
-        if (mVideoView != null) {
-            mVideoView.start();
-        }
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        mVideoView.stopPlayback();
+        if (mExoPlayer != null) {
+            mExoPlayer.stop();
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        if (mVideoView != null) {
-            mVideoView = null;
+        if (mExoPlayer != null) {
+            mExoPlayer.release();
         }
     }
 
@@ -122,7 +120,7 @@ public class PlayerActivity extends AppCompatActivity implements MediaPlayer.OnP
             return;
         }
 
-        mVideoView.setVideoURI(Uri.parse(url));
+        prepareVideo(url);
     }
 
     public void onPlayerLoadedFailed() {
@@ -142,35 +140,17 @@ public class PlayerActivity extends AppCompatActivity implements MediaPlayer.OnP
         }
     }
 
-    @Override
-    public boolean onError(MediaPlayer mp, int what, int extra) {
-        Toast.makeText(this, "Khong the tai video", Toast.LENGTH_LONG).show();
+    public void prepareVideo(String url) {
+        DefaultBandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "OnePieceLover"),
+                bandwidthMeter);
+
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+        MediaSource videoSource = new ExtractorMediaSource(Uri.parse(url),
+                dataSourceFactory, extractorsFactory, null, null);
+        mExoPlayer.prepare(videoSource);
+
         showLoading(false);
-        finish();
-
-        return true;
-    }
-
-    @Override
-    public void onPrepared(MediaPlayer mp) {
-        showLoading(false);
-        mVideoView.start();
-    }
-
-    private void hideSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
-    }
-
-    private void showSystemUI() {
-        getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
     }
 }
